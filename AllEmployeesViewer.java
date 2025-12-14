cb8ee078a27c9d06babad9507c62c45d2e88299c
faizaanmd2908-design <faizaanmd2908@gmail.com>
2025-12-14 13:19:36 +0530

    Count total employees in viewAll method
    Count total employees while reading from Data.txt.

--- AllEmployeesViewer.java
+++ AllEmployeesViewer.java
@@ -6,10 +6,18 @@ public class AllEmployeesViewer {
 
     public void viewAll() throws IOException {
         System.out.println("============ ALL EMPLOYEES ============");
+
+        int totalEmployees = 0;
+
         try (BufferedReader br = new BufferedReader(new FileReader("Data.txt"))) {
             String line;
-            while ((line = br.readLine()) != null) System.out.println(line);
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                totalEmployees++;
+            }
         }
+
+        System.out.println("Total Employees : " + totalEmployees);
         System.out.println("=======================================");
     }
 }
